721d7ee8ae698193f681ba8b02397625af7174c1
youngwoo-elec <youngwoo.elec@gmail.com>
2026-05-01 07:32:42 +0000

    Bug: runtime error

---
+++ groom/training.c
@@ -0,0 +1,17 @@
+//대소문자 바꾸기
+#include <stdio.h>
+
+int main(){
+	int len;
+	char input[100];
+	scanf("%d", &len);
+	scanf(" %s", input);
+	for(int i=0;i<len;i++){
+		if(input[i] >= 'A' && input[i] <= 'Z')
+			input[i]+= 32;
+		else
+			input[i]-= 32;
+	}
+	printf("%s\n", input);
+	return 0;	
+}
