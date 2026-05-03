ad52634a703882eefd91077d15d0e064aff577f6
youngwoo-elec <youngwoo.elec@gmail.com>
2026-05-03 02:00:15 +0000

    Feat: filter out integers containing the substring K from the array

--- groom/training.c
+++ groom/training.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 //대소문자 바꾸기
 void training_2(){
@@ -27,7 +28,33 @@ int training_5(){
 	return 0;
 }
 
+//숫자 제거 배열
+int training_6(){
+	    int N;
+    char K[20];   
+    if (scanf("%d %s", &N, K) != 2) return 0;
+	
+    int arr[N];      
+    int count = 0;   
+    int input_val;
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &input_val);
+
+        char temp[20];
+        sprintf(temp, "%d", input_val);
+
+        if (strstr(temp, K) == NULL) {
+            arr[count] = input_val;
+            count++;
+        }
+    }
+	
+		printf("%d", count);
+		return 0;
+}
+
 int main(){
-	training_5();
+	training_6();
 	return 0;
 }
